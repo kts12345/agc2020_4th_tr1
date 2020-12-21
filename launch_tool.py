68a23e055040c73ed052702bd5a6ed204ff0b5e2
kts12345 <kts123@estsoft.com>
2020-12-22 01:05:03 +0900

    코드 주석 추가

--- launch_tool.py
+++ launch_tool.py
@@ -11,18 +11,3 @@ def run_test(config_file, checkpoint_file,
     """
     os.system(cmd)
     print('inference completed!!')
-    
-def foo(param):
-    run_test(**param)
-    
-def run_test_mulit(params):
-    pool = mp.Pool(processes=2)
-    pool.map(foo, params)
-    pool.join()
-    pool.close()
-    
-if __name__ == '__main__':
-    import sys
-    print(sys.argv)
-    run_test(*(sys.argv[1:]))
-    
